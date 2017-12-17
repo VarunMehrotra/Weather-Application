98e55bb66e1b2275b5e626afb8e870399760d2a1
VarunMehrotra <varunmehrotra180689@gmail.com>
2017-12-17 00:16:35 -0600

    Initial commit

---
+++ app/src/main/java/com/example/varunmehrotra/weatherapplication/WeatherActivityFragment.java
@@ -0,0 +1,22 @@
+package com.example.varunmehrotra.weatherapplication;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class WeatherActivityFragment extends Fragment {
+
+    public WeatherActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_weather, container, false);
+    }
+}
